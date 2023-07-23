9d95294619e1848420ae0592ac40fddd057604ef
optimuim97 <aboubakar.ouattara@uvci.edu.ci>
2023-07-23 23:27:56 +0000

    update quick service >=>

--- app/Http/Controllers/API/CreateQuickServiceApiController.php
+++ app/Http/Controllers/API/CreateQuickServiceApiController.php
@@ -17,18 +17,69 @@ class CreateQuickServiceApiController extends AppBaseController
     {
         $this->quickServiceRepository = $quickServiceRepo;
     }
+
     /**
-     * Handle the incoming request.
+     * @OA\Post(
+     *      path="/auth/quick-services",
+     *      summary="createQuickService",
+     *      tags={"QuickService"},
+     *      description="Create QuickService",
+     *      @OA\Parameter(
+     *          name="Authorization",
+     *          in="header",
+     *          required=true,
+     *          @OA\Schema(type="string")
+     *        ),
+     *      @OA\RequestBody(
+     *        required=true,
+     *        @OA\JsonContent(ref="#/components/schemas/QuickService"),
+     *        @OA\MediaType(
+     *          mediaType="multipart/form-data",
+     *          @OA\Schema( 
+     *              required={"service_id,address,lat,lon,user_id,duration,is_confirmed,has_already_send_remeber"},
+     *              @OA\Property(property="service_id", type="string"),
+     *              @OA\Property(property="address", type="string"),
+     *              @OA\Property(property="lat", type="string"),
+     *              @OA\Property(property="lon", type="string"),
+     *              @OA\Property(property="user_id", type="string"),
+     *              @OA\Property(property="payment_method_id", type="integer"),
+     *              @OA\Property(property="payment_method_type_id", type="integer"),
+     *              @OA\Property(property="duration", type="string"),
+     *              @OA\Property(property="is_confirmed", type="string"),
+     *              @OA\Property(property="has_already_send_remeber", type="string")
+     *          )
+     *        )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(
+     *                  property="success",
+     *                  type="boolean"
+     *              ),
+     *              @OA\Property(
+     *                  property="data",
+     *                  ref="#/components/schemas/QuickService"
+     *              ),
+     *              @OA\Property(
+     *                  property="message",
+     *                  type="string"
+     *              )
+     *          )
+     *      )
+     * )
      */
     public function __invoke(Request $request)
     {
         $input = $request->all();
         $user = auth("api")->user();
 
-        if($user == null){
+        if ($user == null) {
             return $this->sendError("Votre session a expire", 401);
         }
-        
+
         $input["user_id"] = $user->id;
         $request->validate(QuickService::$rules);
         $currentDate = Carbon::now();
@@ -37,24 +88,24 @@ class CreateQuickServiceApiController extends AppBaseController
         $date = Carbon::parse($input["date"]);
         $checkDate = $currentDate->diffInHours($date, true);
 
-        if($checkDate <=3){
+        if ($checkDate <= 3) {
             return $this->sendError("Le RDV doit etre fixe avec 3 heures d'avances", 422);
         }
         $input["date"] = $date;
 
         $latitude = $input["lat"];
         $longtitude = $input["lon"];
-        
+
         $nearlySalons = $this->getSalon(
             $latitude,
             $longtitude
         );
 
-        if(!empty($nearlySalons)){
+        if (!empty($nearlySalons)) {
 
             $quickService = $this->quickServiceRepository->create($input);
 
-            if($quickService){
+            if ($quickService) {
                 return $this->sendResponse([
                     $quickService->toArray()
                 ], 'Service Rapide enregister avec success');
@@ -63,23 +114,23 @@ class CreateQuickServiceApiController extends AppBaseController
 
             return $this->sendError("Reservation non pris en compte");
         }
-        
+
         return $this->sendError("Pas de salon a proximite");
     }
 
     public function getSalon($latitude, $longtitude)
     {
         return DB::table("salons")
-        ->join("salon_addresses", "salons.id", "=", "salon_addresses.salon_id")
-        ->select(
-            "*",
-            DB::raw("55555 * acos(cos(radians(" . $latitude . ")) 
+            ->join("salon_addresses", "salons.id", "=", "salon_addresses.salon_id")
+            ->select(
+                "*",
+                DB::raw("55555 * acos(cos(radians(" . $latitude . ")) 
             * cos(radians(salon_addresses.lat)) 
             * cos(radians(salon_addresses.lon) - radians(" . $longtitude . ")) 
             + sin(radians(" . $latitude . ")) 
             * sin(radians(salon_addresses.lat))) AS distance")
-        )
-        ->groupBy("salon_addresses.id")
-        ->get();
+            )
+            ->groupBy("salon_addresses.id")
+            ->get();
     }
 }

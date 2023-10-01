2ed2baaf361b0da48a3690bc783260c221e0847b
optimuim97 <assidikouattara@gmail.com>
2023-10-01 19:23:52 +0000

    salon service ...

---
+++ app/Http/Controllers/userSearchController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class UserSearchController extends AppBaseController
+{
+    public function searchByEmail(Request $request)
+    {
+        $email = $request->query('email');
+        $user = User::where('email', $email)->first();
+
+        if (!empty($user)) {
+            return response()->json([
+                "message" => "user retreived",
+                "status_code" => Response::HTTP_FOUND,
+                "data" => $user
+            ], Response::HTTP_FOUND);
+        } else {
+            return response()->json([
+                "message" => "Not found",
+                "status_code" => Response::HTTP_NOT_FOUND,
+            ], Response::HTTP_OK);
+        }
+    }
+
+    public function searchByPhone(Request $request)
+    {
+        $email = $request->query('phone_number');
+        $user = User::where('phone_number', $email)->first();
+
+        if (!empty($user)) {
+            return response()->json([
+                "message" => "user retreived",
+                "status_code" => Response::HTTP_FOUND,
+                "data" => $user
+            ], Response::HTTP_FOUND);
+        } else {
+            return response()->json([
+                "message" => "Not found",
+                "status_code" => Response::HTTP_NOT_FOUND,
+            ], Response::HTTP_OK);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\API\QuickService\CreateQuickServiceApiController;
 use App\Http\Controllers\API\QuickService\GetServiceByTypeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SearchSalonController;
 use App\Http\Controllers\SignUpController;
 use App\Http\Controllers\testController;
+use App\Http\Controllers\UserSearchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 // use Imgur;
@@ -32,6 +34,14 @@ Route::group([
 Route::post('auth/request-quick-service', CreateQuickServiceApiController::class);
 Route::get('get-service-by-type/{id}', GetServiceByTypeController::class);
 Route::post('sign-up', [SignUpController::class, 'register']);
+
+Route::get('users/info_by_email', [UserSearchController::class, 'searchByEmail']);
+Route::get('users/info_by_phone_number', [UserSearchController::class, 'searchByPhone']);
+
+Route::get('salons/search_by_name', [SearchSalonController::class, 'searchByName']);
+Route::get('salons/search_by_address_name', [SearchSalonController::class, 'searchByAddressName']);
+
+
 Route::post('test', [testController::class, 'test']);
 
 Route::resource('salons', App\Http\Controllers\API\SalonAPIController::class)
@@ -57,8 +67,8 @@ Route::resource('salon-service-types', App\Http\Controllers\API\SalonServiceType
 Route::resource('service-place-types', App\Http\Controllers\API\ServicePlaceTypeAPIController::class)
     ->except(['create', 'edit']);
 
-// Route::resource('salon-services', App\Http\Controllers\API\SalonServiceAPIController::class)
-//     ->except(['create', 'edit']);
+Route::resource('salon-services', App\Http\Controllers\API\SalonServiceAPIController::class)
+    ->except(['create', 'edit']);
 
 Route::resource('salon-pictures', App\Http\Controllers\API\SalonPictureAPIController::class)
     ->except(['create', 'edit']);

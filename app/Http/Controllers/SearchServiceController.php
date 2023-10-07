f524db608e823602003ac1c519b3fd3d74603392
optimuim97 <assidikouattara@gmail.com>
2023-10-07 13:39:27 +0000

    service_type_id

--- app/Http/Controllers/SearchServiceController.php
+++ app/Http/Controllers/SearchServiceController.php
@@ -30,7 +30,7 @@ class SearchServiceController extends Controller
 
     public function searchSalonServiceByType(Request $request)
     {
-        $serviceType = $request->query('service_type_id');
+        return $serviceType = $request->query('service_type_id');
         $service = SalonService::where('service_type_id', $serviceType)->first();
 
         if (!empty($service)) {

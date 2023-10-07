fe2d989199ff577dafe1693431a2fb94e5735a8e
optimuim97 <assidikouattara@gmail.com>
2023-10-07 13:34:09 +0000

    add Salon address ...

--- app/Http/Controllers/SearchServiceController.php
+++ app/Http/Controllers/SearchServiceController.php
@@ -50,7 +50,7 @@ class SearchServiceController extends Controller
     public function searchServiceByType(Request $request)
     {
         $serviceType = $request->query('service_type_id');
-        $service = Service::where('service_type_id', $serviceType)->first();
+        $service = Service::where('id', $serviceType)->first();
 
         if (!empty($service)) {
             return response()->json([

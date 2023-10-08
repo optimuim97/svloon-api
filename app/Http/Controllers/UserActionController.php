61025ededb2f9aa91f5edef72d48ba0f579234a0
optimuim97 <assidikouattara@gmail.com>
2023-10-08 15:24:39 +0000

    update user info...

--- app/Http/Controllers/UserActionController.php
+++ app/Http/Controllers/UserActionController.php
@@ -8,25 +8,25 @@ class UserActionController extends AppBaseController
 {
     public function updateUser(Request $request)
     {
-        dd($request->all());
+
         $user = auth("api")->user();
 
         if (empty($user)) {
             return $this->sendResponse($user, 'User must be connected');
         }
 
-        $user->phone_number = $request->email == "" ? $user->email : $request->email;
+        $user->lastname = $request->email == "" ? $user->email : $request->email;
         // $user->phone_number = $request->password == "" ? $user->password : $request->password;
-        $user->phone_number = $request->firstname == "" ? $user->firstname : $request->firstname;
-        $user->phone_number = $request->lastname == "" ? $user->lastname : $request->lastname;
+        $user->lastname = $request->firstname == "" ? $user->firstname : $request->firstname;
+        $user->lastname = $request->lastname == "" ? $user->lastname : $request->lastname;
         // $user->phone_number = $request->name == "" ? $user->name : $request->name;
-        $user->phone_number = $request->dial_code == "" ? $user->dial_code : $request->dial_code;
-        $user->phone_number = $request->phone_number == "" ? $user->phone_number : $request->phone_number;
-        $user->phone_number = $request->profession == "" ? $user->profession : $request->profession;
-        $user->phone_number = $request->photo_url == "" ? $user->photo_url : $request->photo_url;
-        $user->phone_number = $request->is_active == "" ? $user->is_active : $request->is_active;
-        $user->phone_number = $request->is_professional == "" ? $user->is_professional : $request->is_professional;
-        $user->phone_number = $request->email == "" ? $user->email : $request->email;
+        $user->lastname = $request->dial_code == "" ? $user->dial_code : $request->dial_code;
+        $user->lastname = $request->phone_number == "" ? $user->phone_number : $request->phone_number;
+        $user->lastname = $request->profession == "" ? $user->profession : $request->profession;
+        $user->lastname = $request->photo_url == "" ? $user->photo_url : $request->photo_url;
+        $user->lastname = $request->is_active == "" ? $user->is_active : $request->is_active;
+        $user->lastname = $request->is_professional == "" ? $user->is_professional : $request->is_professional;
+        $user->lastname = $request->email == "" ? $user->email : $request->email;
 
         // dd($user);
         // $user->phone_number = $request->email_verified_at == "" ? $user->email_verified_at : $request->email_verified_at;

f85c7ec5799d0ed273c48f4148fa21ab373fb657
optimuim97 <assidikouattara@gmail.com>
2023-11-27 17:27:51 +0000

    test callback ..

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -8,20 +8,20 @@ use Illuminate\Http\Request;
 
 class testController extends Controller
 {
-    function test(Request $request)
-    {
-        if ($request->file('photo_url')) {
-            $image = $request->file('photo_url');
-            if ($image != null) {
-                $finalImage = Imgur::upload($image);
-                $finalImageLink = $finalImage->link();
-            }
-        } else {
-            $finalImageLink = 'https://i.imgur.com/zCL2LAh.png';
-        }
+    // function test(Request $request)
+    // {
+    //     if ($request->file('photo_url')) {
+    //         $image = $request->file('photo_url');
+    //         if ($image != null) {
+    //             $finalImage = Imgur::upload($image);
+    //             $finalImageLink = $finalImage->link();
+    //         }
+    //     } else {
+    //         $finalImageLink = 'https://i.imgur.com/zCL2LAh.png';
+    //     }
 
-        return $finalImageLink;
-    }
+    //     return $finalImageLink;
+    // }
 
     public  function callBack(Request $request){
         $input = $request->all();
